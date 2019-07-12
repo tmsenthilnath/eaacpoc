608e6c44e5e116a101c6e1e7e15cf37ea9e3f590
tmsenthilnath <52785592+tmsenthilnath@users.noreply.github.com>
2019-07-12 18:43:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+variable "azure_client_id" {
+description = "Client ID for the Service Principal."
+}
+variable "azure_client_secret" {
+description = "Client Secret for the Service Principal."
+}
 provider "azurerm" {
 subscription_id = "38a48eb9-12be-4a21-91d8-d92ce3ad56a4"
 client_id = "7b3c9147-a5ef-48fb-9a67-bdf769763926"
